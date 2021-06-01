a44030c2b7213f4faa2e09475e236d487b0681dd
= <=>
2021-06-01 14:12:32 +0700

    make it better

--- src/main/java/NET/taovan/NoteRepository.java
+++ src/main/java/NET/taovan/NoteRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NoteRepository extends JpaRepository<Notes, Long> {
 	 	@Query("SELECT n FROM Notes n WHERE lower(n.tieude) lIKE %?1%"
-+ " OR n.tieude lIKE %?1%"+ " OR upper(n.tieude) lIKE %?1%")
++ "OR n.tieude lIKE %?1%"+ " OR upper(n.tieude) lIKE %?1%")
 	 	public List<Notes>findAll(String keyword);
 //		public Notes findByTieude(String keyword);
 }
